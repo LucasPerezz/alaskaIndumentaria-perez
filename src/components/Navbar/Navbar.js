b0b8a7e70d9ae38e63f2a4cfd3508fe74cc2652d
LucasPerezz <perezlucas2609@gmail.com>
2021-11-14 03:23:12 -0300

    proyecto react

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import './Navbar.css'
+
+function Nabvar () {
+    return(
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+    <a class="navbar-brand position-absolute mx-4 tittleFontType" href="#">Alaska Indumentaria</a>
+    <div class="collapse navbar-collapse justify-content-center container-fluid" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active fontType" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active fontType" href="#">Mujer</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active fontType" href="#">Hombre</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active fontType" href="#">Contacto</a>
+        </li>
+      </ul>
+    </div>
+</nav>
+    ) 
+}
+
+
+export default Nabvar
+
