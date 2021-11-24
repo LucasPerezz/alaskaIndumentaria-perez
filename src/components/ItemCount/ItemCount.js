7306574c9d798086fc57600ad13f0b5f2f4076fb
LucasPerezz <perezlucas2609@gmail.com>
2021-11-23 21:25:43 -0300

    DESAFIO COMPONENTES II

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,39 @@
+import React, { Fragment, useState } from "react";
+import { Button, Icon } from "semantic-ui-react";
+
+import './ItemCount.css'
+
+const ItemCount = () => {
+  const [counter, setCounter] = useState(1)
+
+
+  const handlerCounterUp = () => {
+    if(counter != 0) {
+
+      setCounter (counter + 1)
+    }
+  }
+
+  const handlerCounterDown = () => {
+    if(counter != 1) {
+      
+      setCounter (counter - 1)
+    }
+  }
+
+  return (
+    <Fragment>
+      <div className="counter">
+      <Button icon onClick={handlerCounterDown}>
+        <Icon name="minus"/>
+      </Button>
+        <p>{counter}</p>
+      <Button icon  onClick={handlerCounterUp}>
+        <Icon name="plus" />
+      </Button>
+      </div>
+    </Fragment>
+  );
+};
+
+export default ItemCount;
