5b90b810ced2493c1e134294000b04176ecc4d02
LucasPerezz <perezlucas2609@gmail.com>
2022-01-06 03:18:56 -0300

    entrega proyecto

--- src/components/services/getFirestore.js
+++ src/components/services/getFirestore.js
@@ -1,16 +1,15 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase";
+import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_APPID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+  apiKey: "AIzaSyBPwxRD2Fe4ObF68v4R4sfm1C0sHPAupZU",
+  authDomain: "alaskaindumentaria-2d869.firebaseapp.com",
+  projectId: "alaskaindumentaria-2d869",
+  storageBucket: "alaskaindumentaria-2d869.appspot.com",
+  messagingSenderId: "442503702671",
+  appId: "1:442503702671:web:6eec1d296e8b13f57068fa",
+  measurementId: "G-D7795L0DTC"
 };
 
 // Initialize Firebase
