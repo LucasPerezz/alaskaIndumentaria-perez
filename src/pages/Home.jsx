f80ea53ee497817bbac07694ac02fe2700aa4a6f
LucasPerezz <perezlucas2609@gmail.com>
2022-06-10 00:53:05 -0300

    proyecto terminado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import './Home.css'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <></>
-    // <div className='homeContainer'>
-    //     <div className='homeGrid'>
-    //         <img src="https://i.ibb.co/3d8TJjp/1.jpg" alt="" className='image1'/>
-
-    //         <img src="https://i.ibb.co/cNC08GP/2.jpg" alt="" className='image2'/>
-
-
-    //         <img src="https://i.ibb.co/dfY20m3/3.jpg" alt="" className='image3'/>
-
-    //         <img src="https://i.ibb.co/yF7CbCR/4.jpg" alt="" className='image4'/> 
-    //     </div>
-    // </div>
+    <>
+        
+        <img src="https://i.ibb.co/jMQCVCt/image-6487327.jpg" alt="" className='imageCon'/>
+        <img src="https://i.ibb.co/3d8TJjp/1.jpg" alt="" className='imageMobile'/>
+      <div className='homeContainer'>
+          <p>Welcome <br /> to <br /> Alaska Indumentaria</p>
+            <button><Link to="/productos">Nuestros Productos</Link></button> 
+      </div>
+    </>
   )
 }
 
